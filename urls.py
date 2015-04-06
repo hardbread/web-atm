7633d53a98f36b4dccedce479ab72d2ecef2ef9c
Sergey Tverdohleb <s.hardbread@gmail.com>
2015-04-06 11:22:14 +0300

    Web ATM version 1.0 (Test task)

--- urls.py
+++ urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from views import logout
+
+handler404 = 'error_handlers.handler404'
+
 urlpatterns = patterns('',
     url(r'^', include('card.urls')),
     url(r'^logout/$', logout, name='logout'),
